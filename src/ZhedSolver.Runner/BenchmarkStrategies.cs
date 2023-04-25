68d228bc5a00ccea1c1da6d1b46906c0244b79ee
Anders Bo Rasmussen <anras573@gmail.com>
2023-04-25 14:55:43 +0200

    Benchmark the Different Strategies.

---
+++ src/ZhedSolver.Runner/BenchmarkStrategies.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using ZhedSolver.Runner.SolveStrategies;
+
+namespace ZhedSolver.Runner;
+
+public class BenchmarkStrategies
+{
+    const string Level11 =
+        """
+----------
+----------
+----------
+----123---
+---1------
+---2------
+---3-----x
+----------
+----------
+----------
+""";
+    
+    [Benchmark]
+    public void Level11_BFS()
+    {
+        Parser.Parse(Level11).Solve(new BfsSolveStrategy());
+    }
+    
+    [Benchmark]
+    public void Level11_DFS()
+    {
+        Parser.Parse(Level11).Solve(new DfsSolveStrategy());
+    }
+    
+    [Benchmark]
+    public void Level11_Permutation()
+    {
+        Parser.Parse(Level11).Solve(new PermutationStrategy());
+    }
+    
+    [Benchmark]
+    public void Level11_ParallelBFS()
+    {
+        Parser.Parse(Level11).Solve(new ParallelBfsSolveStrategy());
+    }
+    
+    [Benchmark]
+    public void Level11_ParallelPermutation()
+    {
+        Parser.Parse(Level11).Solve(new ParallelPermutationStrategy());
+    }
+}
